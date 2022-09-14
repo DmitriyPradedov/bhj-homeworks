843495ce8bc1c5af9ead185eb605286f6fa91dfa
DmitriyPradedov <pradedov72@mail.ru>
2022-09-14 19:36:17 +0700

    скролл переделка

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,20 +1,8 @@
-const reveal = document.querySelectorAll('.reveal');
+const orangeBlocks = document.getElementsByClassName('reveal');
 
-document.addEventListener('scroll', show);
-
-
-function show(event) {
-
-    const viewportHeight = window.innerHeight;
-
-    for (let element of reveal) {
-
-        const elementTop = element.getBoundingClientRect().top;
-        if (elementTop < viewportHeight) {
-            element.classList.add('reveal_active');
-            
-        } else {
-            element.classList.remove('reveal_active');
-        }
-    }
-}
+window.addEventListener('scroll', function() {
+  for (const block of orangeBlocks) {
+    const rect = block.getBoundingClientRect();
+    block.classList.toggle('reveal_active', (rect.top < window.innerHeight) && (rect.bottom > 0));
+  }  
+})
