d9192eca1b88cc940cefed79a27ff977da6a985b
DmitriyPradedov <pradedov72@mail.ru>
2022-09-20 15:38:28 +0700

    всплывающее окно

---
+++ client-state/popup/task.js
@@ -0,0 +1,15 @@
+function chekSubscrib() {
+    const cookies = document.cookie.split('; ')
+    const result = cookies.find(c => c.startsWith('modal'))
+  
+    return result ? decodeURIComponent(result.substr(6)) : null
+  }
+  
+  if (chekSubscrib() != 'closed') {
+    document.getElementById('subscribe-modal').classList.add('modal_active');
+  }
+  
+  document.querySelector('.modal__close').addEventListener('click', () => {
+     document.cookie = 'modal=closed'
+     document.getElementById('subscribe-modal').classList.remove('modal_active');
+  })
